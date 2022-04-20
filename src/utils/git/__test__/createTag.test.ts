4f2ee44b53242527af1eac80c15a61b752c526f6
Artem Zakharchenko <kettanaito@gmail.com>
2022-04-20 13:16:06 +0200

    test: add test for "createTag"

---
+++ src/utils/git/__test__/createTag.test.ts
@@ -0,0 +1,49 @@
+import { createTeardown } from 'fs-teardown'
+import { Git } from 'node-git-server'
+import { createOrigin, initGit, startGitProvider } from '../../../../test/utils'
+import { execAsync } from '../../execAsync'
+import { createTag } from '../createTag'
+
+const origin = createOrigin()
+const fsMock = createTeardown({
+  rootDir: 'tarm/get-tags',
+})
+
+const gitProvider = new Git(fsMock.resolve('git-provider'), {
+  autoCreate: true,
+})
+
+gitProvider.on('push', (push) => push.accept())
+gitProvider.on('fetch', (fetch) => fetch.accept())
+
+beforeAll(async () => {
+  await fsMock.prepare()
+  await startGitProvider(gitProvider, await origin.get())
+  execAsync.mockContext({
+    cwd: fsMock.resolve(),
+  })
+})
+
+beforeEach(async () => {
+  await fsMock.reset()
+  await initGit(fsMock, origin.url)
+  jest.restoreAllMocks()
+})
+
+afterAll(async () => {
+  await fsMock.cleanup()
+  await gitProvider.close()
+  execAsync.restoreContext()
+})
+
+it('creates a new tag', async () => {
+  expect(await createTag('1.0.0')).toBe('1.0.0')
+})
+
+it('does not create a tag when it already exists', async () => {
+  jest.spyOn(console, 'error').mockImplementation()
+  await execAsync('git tag 1.0.0')
+  await expect(createTag('1.0.0')).rejects.toThrow(
+    `fatal: tag '1.0.0' already exists`
+  )
+})
