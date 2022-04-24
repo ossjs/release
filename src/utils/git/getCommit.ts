31235d19d9a632dfb705fa5a652bf66fe8b71a5e
Artem Zakharchenko <kettanaito@gmail.com>
2022-04-24 14:44:00 +0200

    fix(getCommit): respect execAsync cwd

--- src/utils/git/getCommit.ts
+++ src/utils/git/getCommit.ts
@@ -1,5 +1,6 @@
 import * as getStream from 'get-stream'
 import gitLogParser, { Commit } from 'git-log-parser'
+import { execAsync } from '../execAsync'
 
 export async function getCommit(hash: string): Promise<Commit | undefined> {
   const result = await getStream.array<Commit>(
@@ -8,7 +9,11 @@ export async function getCommit(hash: string): Promise<Commit | undefined> {
         _: hash,
         n: 1,
       },
-      {}
+      {
+        // Respect the global working directory so this command
+        // parses commits on test repositories during tests.
+        cwd: execAsync.contextOptions.cwd,
+      }
     )
   )
 
