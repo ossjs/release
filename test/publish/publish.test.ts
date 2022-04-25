fa61a50be8a7abeb8cfe550b0cc1452f52c1bbb1
Artem Zakharchenko <kettanaito@gmail.com>
2022-04-25 02:40:38 +0200

    test(publish): use test environment

--- test/publish/publish.test.ts
+++ test/publish/publish.test.ts
@@ -1,72 +1,39 @@
-import { createTeardown } from 'fs-teardown'
-import { Git } from 'node-git-server'
 import { rest } from 'msw'
-import { setupServer } from 'msw/node'
 import { log } from '../../src/logger'
-import { createOrigin, initGit, startGitProvider } from '../utils'
 import { Publish } from '../../src/commands/publish'
-import { execAsync } from '../../src/utils/execAsync'
 import type { CreateReleaseResponse } from '../../src/utils/git/createRelease'
+import { testEnvironment } from '../env'
 
-const server = setupServer(
-  rest.post<never, never, CreateReleaseResponse>(
-    'https://api.github.com/repos/:owner/:repo/releases',
-    (req, res, ctx) => {
-      return res(
-        ctx.status(201),
-        ctx.json({
-          html_url: '/releases/1',
-        })
-      )
-    }
-  )
-)
-
-const fsMock = createTeardown({
-  rootDir: 'tarm/publish',
-  paths: {
-    'git-provider': null,
-  },
-})
-
-const origin = createOrigin()
-
-const gitProvider = new Git(fsMock.resolve('git-provider'), {
-  autoCreate: true,
-})
-  .on('push', (push) => push.accept())
-  .on('fetch', (fetch) => fetch.accept())
+const { setup, reset, cleanup, fs, api } = testEnvironment('publish')
 
 beforeAll(async () => {
-  jest.spyOn(log, 'info').mockImplementation()
-  jest.spyOn(log, 'error')
-
-  execAsync.mockContext({
-    cwd: fsMock.resolve(),
-  })
-  server.listen({
-    onUnhandledRequest: 'error',
-  })
-  await fsMock.prepare()
-  await startGitProvider(gitProvider, await origin.get())
+  await setup()
 })
 
 afterEach(async () => {
-  jest.resetAllMocks()
-  server.resetHandlers()
-  await fsMock.reset()
+  await reset()
 })
 
 afterAll(async () => {
-  jest.restoreAllMocks()
-  execAsync.restoreContext()
-  server.close()
-  await fsMock.cleanup()
-  await gitProvider.close()
+  await cleanup()
 })
 
 it('publishes the next minor version', async () => {
-  await fsMock.create({
+  api.use(
+    rest.post<never, never, CreateReleaseResponse>(
+      'https://api.github.com/repos/:owner/:repo/releases',
+      (req, res, ctx) => {
+        return res(
+          ctx.status(201),
+          ctx.json({
+            html_url: '/releases/1',
+          })
+        )
+      }
+    )
+  )
+
+  await fs.create({
     'package.json': JSON.stringify({
       name: 'test',
       version: '0.0.0',
@@ -77,8 +44,7 @@ module.exports = {
 }
     `,
   })
-  await initGit(fsMock, origin.url)
-  await fsMock.exec(`git add . && git commit -m 'feat: new things'`)
+  await fs.exec(`git add . && git commit -m 'feat: new things'`)
 
   const publish = new Publish({
     script: 'echo "release script input: $RELEASE_VERSION"',
@@ -100,17 +66,18 @@ module.exports = {
   expect(log.info).toHaveBeenCalledWith('release script input: 0.1.0\n')
 
   // Must bump the "version" in package.json.
-  expect(
-    JSON.parse(await fsMock.readFile('package.json', 'utf8'))
-  ).toHaveProperty('version', '0.1.0')
+  expect(JSON.parse(await fs.readFile('package.json', 'utf8'))).toHaveProperty(
+    'version',
+    '0.1.0'
+  )
 
-  expect(await fsMock.exec('git log')).toHaveProperty(
+  expect(await fs.exec('git log')).toHaveProperty(
     'stdout',
     expect.stringContaining('chore: publish v0.1.0')
   )
 
   // Must create a new tag for the release.
-  expect(await fsMock.exec('git tag')).toHaveProperty(
+  expect(await fs.exec('git tag')).toHaveProperty(
     'stdout',
     expect.stringContaining('0.1.0')
   )
