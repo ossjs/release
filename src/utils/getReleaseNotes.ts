d285c977abe3033bc045b759e8166633a45e5039
Artem Zakharchenko <kettanaito@gmail.com>
2022-05-12 17:58:14 +0200

    feat(getReleaseNotes): add "BREAKING CHANGES" section

--- src/utils/getReleaseNotes.ts
+++ src/utils/getReleaseNotes.ts
@@ -1,18 +1,17 @@
 import type { ReleaseContext } from './createContext'
 import { formatDate } from './formatDate'
+import { isBreakingChange } from './getNextReleaseType'
 import type { ParsedCommitWithHash } from './git/parseCommits'
 
-export type ReleaseNotes = Map<string, Set<ParsedCommitWithHash>>
+export type ReleaseNoteType = 'breaking' | 'feat' | 'fix'
+export type ReleaseNotes = Map<ReleaseNoteType, Set<ParsedCommitWithHash>>
 
 const IGNORE_COMMIT_TYPE = ['chore']
 
 export async function getReleaseNotes(
   commits: ParsedCommitWithHash[],
 ): Promise<ReleaseNotes> {
-  const releaseNotes: ReleaseNotes = new Map<
-    string,
-    Set<ParsedCommitWithHash>
-  >()
+  const releaseNotes: ReleaseNotes = new Map()
 
   for (const commit of commits) {
     const { type, merge } = commit
@@ -21,9 +20,13 @@ export async function getReleaseNotes(
       continue
     }
 
+    const noteType: ReleaseNoteType = isBreakingChange(commit)
+      ? 'breaking'
+      : (type as ReleaseNoteType)
+
     const nextCommits =
-      releaseNotes.get(type) || new Set<ParsedCommitWithHash>()
-    releaseNotes.set(type, nextCommits.add(commit))
+      releaseNotes.get(noteType) || new Set<ParsedCommitWithHash>()
+    releaseNotes.set(noteType, nextCommits.add(commit))
   }
 
   return releaseNotes
@@ -38,25 +41,31 @@ export function toMarkdown(
 
   markdown.push(`## ${context.nextRelease.tag} (${releaseDate})`)
 
-  const sections: Record<'feat' | 'fix', string[]> = {
+  const sections: Record<ReleaseNoteType, string[]> = {
+    breaking: [],
     feat: [],
     fix: [],
   }
 
-  for (const [commitType, commits] of notes) {
-    const section = sections[commitType as 'feat' | 'fix']
+  for (const [noteType, commits] of notes) {
+    const section = sections[noteType]
 
     if (!section) {
       continue
     }
 
     for (const commit of commits) {
-      const releaseItem = createReleaseItem(commit)
+      const releaseItem = createReleaseItem(commit, noteType === 'breaking')
 
       if (releaseItem) {
-        section.push(releaseItem)
+        section.push(...releaseItem)
       }
     }
+  }
+
+  if (sections.breaking.length > 0) {
+    markdown.push('', '### ⚠️ BREAKING CHANGES')
+    markdown.push(...sections.breaking)
   }
 
   if (sections.feat.length > 0) {
@@ -72,14 +81,32 @@ export function toMarkdown(
   return markdown.join('\n')
 }
 
-function createReleaseItem(commit: ParsedCommitWithHash): string | undefined {
+function createReleaseItem(
+  commit: ParsedCommitWithHash,
+  includeNotes: boolean = false,
+): string[] {
   const { subject, scope, hash } = commit
 
-  if (subject) {
-    const commitLine = [scope && `**${scope}:**`, subject, `(${hash})`]
-      .filter(Boolean)
-      .join(' ')
-
-    return `- ${commitLine}`
+  if (!subject) {
+    return []
   }
+
+  const commitLine: string[] = [
+    ['-', scope && `**${scope}:**`, subject, `(${hash})`]
+      .filter(Boolean)
+      .join(' '),
+  ]
+
+  if (includeNotes) {
+    const notes = commit.notes.reduce<string[]>((all, note) => {
+      return all.concat('', note.text)
+    }, [])
+
+    if (notes.length > 0) {
+      commitLine.unshift('')
+      commitLine.push(...notes)
+    }
+  }
+
+  return commitLine
 }
