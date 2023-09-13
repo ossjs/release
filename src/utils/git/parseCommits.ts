c118403eb9695db4b2b80d2ab7ebfc98f01c0920
Artem Zakharchenko <kettanaito@gmail.com>
2023-09-13 19:44:06 +0200

    chore(parseCommits): use deferred promise for stream handling

--- src/utils/git/parseCommits.ts
+++ src/utils/git/parseCommits.ts
@@ -2,6 +2,7 @@ import { PassThrough } from 'stream'
 import type { Commit } from 'git-log-parser'
 import parseCommit from 'conventional-commits-parser'
 import type { Commit as ParsedCommit } from 'conventional-commits-parser'
+import { DeferredPromise } from '@open-draft/deferred-promise'
 
 export type ParsedCommitWithHash = ParsedCommit & {
   hash: string
@@ -23,31 +24,33 @@ export async function parseCommits(
   through.end()
 
   const commitParser = parseCommit()
-  const results = await new Promise<ParsedCommitWithHash[]>(
-    (resolve, reject) => {
-      const commits: ParsedCommitWithHash[] = []
 
-      through
-        .pipe(commitParser)
-        .on('error', (error) => reject(error))
-        .on('data', (parsedCommit: ParsedCommit) => {
-          const { header } = parsedCommit
+  const parsingStreamPromise = new DeferredPromise<
+    Array<ParsedCommitWithHash>
+  >()
+  parsingStreamPromise.finally(() => {
+    through.destroy()
+  })
 
-          if (!header) {
-            return
-          }
+  const parsedCommits: ParsedCommitWithHash[] = []
 
-          const originalCommit = commitMap[header]
-          const commit: ParsedCommitWithHash = Object.assign({}, parsedCommit, {
-            hash: originalCommit.hash,
-          })
-          commits.push(commit)
-        })
-        .on('end', () => resolve(commits))
-    },
-  )
+  through
+    .pipe(commitParser)
+    .on('error', (error) => parsingStreamPromise.reject(error))
+    .on('data', (parsedCommit: ParsedCommit) => {
+      const { header } = parsedCommit
 
-  through.destroy()
+      if (!header) {
+        return
+      }
 
-  return results
+      const originalCommit = commitMap[header]
+      const commit: ParsedCommitWithHash = Object.assign({}, parsedCommit, {
+        hash: originalCommit.hash,
+      })
+      parsedCommits.push(commit)
+    })
+    .on('end', () => parsingStreamPromise.resolve(parsedCommits))
+
+  return parsingStreamPromise
 }
