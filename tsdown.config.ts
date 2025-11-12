fbd54ff294123afb660af3569fdf595210de89c1
Artem Zakharchenko <kettanaito@gmail.com>
2025-11-12 12:12:33 +0100

    fix: update dependencies (#76)
    * fix: update dependencies
    
    * fix: update exports extensions to `.mjs`
    
    * chore: update `vitest`

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['./src/**/*.ts', '!**/__test__'],
   format: 'esm',
+  fixedExtension: true,
   outDir: './bin/build/',
   tsconfig: './tsconfig.src.json',
   unbundle: true,
