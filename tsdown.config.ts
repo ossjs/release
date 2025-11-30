1dcfdc48acc20a2d58b170eeb806f250f1c0f3c2
Artem Zakharchenko <kettanaito@gmail.com>
2025-11-30 19:20:06 +0100

    chore: use .js and .d.ts extensions

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['./src/**/*.ts', '!**/__test__'],
   format: 'esm',
-  fixedExtension: true,
+  outExtensions: () => ({
+    js: '.js',
+    dts: '.d.ts',
+  }),
   outDir: './bin/build/',
   tsconfig: './tsconfig.src.json',
   unbundle: true,
